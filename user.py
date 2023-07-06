f3d7d3003be362356dd268e2d0f63be95edb49e4
Muhammad Khosravi <mohamad138121@gmail.com>
2023-07-06 19:11:27 +0330

    add feature

--- user.py
+++ user.py
@@ -7,11 +7,10 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
     first_name = Column(String(50))
     last_name = Column(String(50))
     username = Column(String(50), primary_key=True)
-    favorite_poet = Column(String(10))
+    favorite_poet = Column(String(10), default="1")
 
 
 def get_base():
