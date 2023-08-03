0b61c3a4675dd1503900e8f735c567107de3a491
Muhammad Khosravi <mohamad138121@gmail.com>
2023-08-03 15:40:49 +0330

    added some tweaks

--- app/main.py
+++ app/main.py
@@ -146,7 +146,7 @@ def initialize_logger():
 
 
 def run_scheduler():
-    scheduled_time = get_time(hour=22, minute=0, second=0)
+    scheduled_time = get_time(hour=18, minute=0, second=0)
     schedule.every().day.at(str(scheduled_time)).do(send_poem_to_all_users)
 
     while True:
